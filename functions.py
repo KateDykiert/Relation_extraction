2696bc0516f994b2defa10a1ddb0803b9ef953bb
Piotr Biskup <pbiskup@egnyte.com>
2020-01-28 20:25:20 +0100

    final commit

--- functions.py
+++ functions.py
@@ -1,6 +1,14 @@
-import nltk, re, pprint
+import string
+from nltk.corpus import stopwords
+import nltk, re
+import itertools
+from nltk.stem import PorterStemmer
 
-def parse_task_1_2(file, only_sentences=False):
+ps = PorterStemmer()
+stop_words = list(set(stopwords.words('english')))
+
+
+def parse_file(file, only_sentences=False):
     with open(file, 'r') as file:
         data = file.read().split('\n\n')
         is_string_re = 'nif:isString.*\"(.*)\"'
@@ -23,7 +31,77 @@ def parse_task_1_2(file, only_sentences=False):
     else:
         return meta, sentences
 
+
 def ie_preprocess(document):
     sentences = [nltk.word_tokenize(sent) for sent in document]
     sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
+
+
+def process_using_spacy(sent, en_core, format_result):
+    entities = en_core(sent).ents
+    if format_result:
+        result = list(
+            zip([e.text for e in entities], [e.label_ for e in entities]))
+    else:
+        result = entities
+    return result
+
+
+big_dict = {
+    ('Organisation', 'Organisation'): ['affiliation'],
+    ('Person', 'EducationalInstitution'): ['almaMater'],
+    ('Band', 'Person'): ['formerBandMember', 'bandMember'],
+    ('Person', 'Place'): ['deathPlace', 'birthPlace'],
+    ('Organisation', 'Person'): ['president', 'ceo'],
+    ('Person', 'Person'): ['spouse', 'relative', 'parent', 'child'],
+    ('Athlete', 'SportsTeam'): ['formerTeam', 'debutTeam', 'club'],
+    ('Organisation', 'Country'): ['country'],
+    ('Person', 'Country'): ['country'],
+    ('Place', 'Country'): ['country'],
+    ('PopulatedPlace', 'PopulatedPlace'): ['department'],
+    ('Place', 'PopulatedPlace'): ['district'],
+    ('Scientist', 'Person'): ['doctoralStudent', 'doctoralAdvisor'],
+    ('Person', 'Organisation'): ['employer'],
+    ('Organisation', 'City'): ['foundationPlace'],
+    ('Organisation', 'PopulatedPlace'): ['headquarter'],
+    ('Organisation', 'Settlement'): ['hometown'],
+    ('Person', 'Settlement'): ['hometown'],
+    ('PopulatedPlace', 'Person'): ['leaderName'],
+    ('Place', 'Place'): ['locatedInArea'],
+    ('Organisation', 'Place'): ['location'],
+    ('Person', 'Place'): ['location'],
+    ('Place', 'Place'): ['location'],
+    ('Person', 'Country'): ['nationality'],
+    ('Company', 'Company'): ['subsidiary'],
+    ('ArchitecturalStructure', 'Organisation'): ['tenant'],
+    ('Athlete', 'Person'): ['trainer'],
+    ('Organisation', 'Date'): ['when'],
+    ('Date', 'Organisation'): ['when'],
+    ('Person', 'Date'): ['when'],
+    ('Date', 'Person'): ['when']
+}
+
+maps = {
+    'ORG': 'Organisation',
+    'PERSON': 'Person',
+    'GPE': 'Place',
+    'DATE': 'Date'
+}
+
+
+def get_relation(pairs, sent):
+    perm = list(itertools.permutations(pairs, 2))
+
+    sent = sent.translate(str.maketrans('', '', string.punctuation))
+    sent = [ps.stem(w) for w in nltk.word_tokenize(sent) if not w in stop_words]
+
+    rel = []
+    for a, b in perm:
+        if a[1] in maps.keys() and b[1] in maps.keys():
+            if (maps[a[1]], maps[b[1]]) in big_dict:
+                co = big_dict[(maps[a[1]], maps[b[1]])]
+                for c in co:
+                    if ps.stem(c) in sent:
+                        rel.append((a, b, c))
+    return rel
--- main.py
+++ main.py
@@ -1,86 +1,76 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import json
+
+import en_core_web_sm
 import itertools
 
-from SPARQLWrapper import SPARQLWrapper, JSON, XML
-import json
 import functions as f
-import os, nltk, re
-#nltk.download('punkt')
-#nltk.download('averaged_perceptron_tagger')
-#nltk.download('universal_tagset')
-#nltk.download('maxent_ne_chunker')
-#nltk.download('words')
-#nltk.download('ace')
 
 sentence_list = []
 basepath = 'training/'
-for entry in os.listdir(basepath):
-    if os.path.isfile(os.path.join(basepath, entry)):
-        sent, sentences = f.parse_task_1_2(basepath+entry, only_sentences=True)
-        sentence_list.append(sent)
 
-print(sentence_list[0:10])
-tokens = f.ie_preprocess(sentence_list)
-grammar3 = "PERSON: {<PERSON><PERSON>+}"
-cp = nltk.RegexpParser(grammar3)
+# for entry in os.listdir(basepath):
+#     if os.path.isfile(os.path.join(basepath, entry)):
+#         sent, sentences = f.parse_file(basepath + entry, only_sentences=True)
+#         sentence_list.append(sent)
+#         # print(sent)
+
+
+sent, sentences = f.parse_file('training/file5.ttl', only_sentences=True)
+print(sent)
+sentence_list.append(sent)
 
 chunks = []
-for t in tokens:
-    chunk = nltk.ne_chunk(t)
-    #chunk2 = cp.parse(chunk)
-    chunks.append(chunk)
+core = en_core_web_sm.load()
 
-print(chunks[0:10])
-founds = []
-for chunk in chunks:
-    s = []
-    for i in chunk.subtrees(filter=lambda x: x.label() == 'PERSON' or x.label() == 'ORGANIZATION' or x.label() == 'GPE'):
-        count = 0
-        for val in i:
-            if count == 0 :
-                #print(val[0])
-                s.append(val[0])
-                count = 1
-            else:
-                #print(val[0])
-                s[len(s)-1] = s[len(s)-1] + '_' + val[0]
-    founds.append(s)
+for sentence in sentence_list:
+    pairs = f.process_using_spacy(sentence, en_core=core, format_result=True)
+    chunks.append(pairs)
 
-print(founds)
+print('Word with entities --------------------------------')
+print(chunks)
+
+chunks_no_space = []
+for element in chunks:
+    tab = []
+    for chunk in element:
+        tab.append(chunk[0].replace(' ', '_'))
+    chunks_no_space.append(tab)
+
+print('Word with entities --------------------------------')
+print(chunks_no_space)
+
+permutations = []
+for chunk in chunks_no_space:
+    permutations.append(list(itertools.permutations(chunk, 2)))
+#
+print('Permutations --------------------------------')
+print(permutations)
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-perm = []
-for l in founds:
-    perm.append(list(itertools.combinations(l,2)))
-    #perm.append(pm)
-perm = list(itertools.chain(*perm))
-print(perm)
-print(len(perm))
-#perm = list(dict.fromkeys(perm))
-#print(len(perm))
-for i in perm:
-    if len(i) > 1:
-        sparql.setQuery("""SELECT ?relationship
-        WHERE {
-            <http://dbpedia.org/resource/"""+i[0]+""">
-            ?relationship 
-            <http://dbpedia.org/resource/"""+i[1]+""">
-        }""")
-        sparql.setReturnFormat(JSON)
+for perm in permutations:
+    for pair in perm:
+        if len(pair) > 1:
+            sparql.setQuery("""SELECT ?relationship
+            WHERE {
+                <http://dbpedia.org/resource/""" + pair[0] + """>
+                ?relationship
+                <http://dbpedia.org/resource/""" + pair[1] + """>
+            }""")
+            sparql.setReturnFormat(JSON)
 
-        response = sparql.query().convert()
-        if response["results"]["bindings"] != []:
-            for ind, relationship in enumerate(response["results"]["bindings"]):
-                value = response["results"]["bindings"][ind]["relationship"]["value"]
-                if value != "http://dbpedia.org/ontology/wikiPageWikiLink" and value != "http://www.w3.org/2000/01/rdf-schema#seeAlso"\
-                        and value != "http://dbpedia.org/ontology/wikiPageRedirects":
-                    print(i[0] +" & "+ i[1])
-                    print(json.dumps(relationship, sort_keys=True, indent=4))
-                    print("=============================================================================")
+            response = sparql.query().convert()
+            if response["results"]["bindings"]:
+                for relationship in response["results"]["bindings"]:
+                    value = relationship["relationship"]["value"]
+                    if value != "http://dbpedia.org/ontology/wikiPageWikiLink" and value != "http://www.w3.org/2000/01/rdf-schema#seeAlso" \
+                            and value != "http://dbpedia.org/ontology/wikiPageRedirects":
+                        print(pair[0] + " & " + pair[1])
+                        print(json.dumps(relationship, sort_keys=True, indent=4))
+                        print("=============================================================================")
 
-#def findRelation(json, name):
-#    for dict in json["results"]["bindings"]:
-#        if str(dict["o"]["value"]).find(name) != -1:
-#            return dict
-#    return "relation not found"
+print("Second solution------------------------------------------------------------------------")
 
-#print(findRelation(response, "James_Damore"))
+for index, sente in enumerate(sentence_list):
+    relation = f.get_relation(chunks[index], sente)
+    print(relation)
